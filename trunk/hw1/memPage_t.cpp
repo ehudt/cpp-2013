94b4f18f86034482f10c12dd13fcb6bf8a7a25f3
zinger.arbel <zinger.arbel@5d74b673-4c72-fe7f-821c-aa23a92225c5>
2013-03-12 16:21:38 +0000

    added stuff to stuff

---
+++ trunk/hw1/memPage_t.cpp
@@ -0,0 +1,86 @@
+#include "memPage_t.h"
+#include <string.h>
+
+memPage_t::maxCapacity = 1024;
+
+memPage_t::memPage_t():
+	size(0),
+	capacity(maxCapacity),
+	position(0),
+	next(NULL),
+	buffer(*(new char[maxCapacity])) {}
+
+memPage_t::memPage_t(const int capacity):
+	size(0),
+	capacity(capacity),
+	position(0),
+	next(NULL),
+	buffer(*(new char[capacity])) {}	
+
+memPage_t::~memPage_t() { 
+	delete[] &buffer;
+}
+
+const int memPage_t::setPosition(const int newPos){
+	if (newPos < 0 || newPos > size) return -1;
+	position = newPos;
+	return position;
+}
+
+const int memPage_t::getPosition(){
+	return position;
+}
+
+const bool memPage_t::isEmpty(){
+	return (size==0);
+}
+
+const bool memPage_t::isFull(){
+	return (size==capacity);
+}
+
+const int memPage_t::getSize(){
+	return size;
+}
+
+const int memPage_t::getCapacity(){
+	return capacity;
+}
+
+const int memPage_t::read(void* const dst, const int sizeToRead, const int offset){
+	if (!dst || sizeToRead <= 0 || offset < 0 || offset >= size) return -1;
+	int actual;
+	actual = min(size-offset, sizeToRead);
+	memcpy(dst, buffer+offset, actual);
+	return actual;
+}
+
+const int memPage_t::read(void* const dst, const int sizeToRead){
+	int actual;
+	actual = read(dst, sizeToRead, position);
+	if (actual != -1) position += actual;
+	return actual;
+}
+
+const int memPage_t::write(const void* const src, const int sizeToWrite, const int offset){
+	if (!src || sizeToWrite <= 0 || offset < 0 || offset > size) return -1;
+	int actual;
+	actual = min(sizeToWrite, capacity-offset);
+	memcpy(buffer+offset, src, actual);
+	return actual;
+}
+
+const int memPage_t::write(const void* const src, const int sizeToWrite){
+	int actual;
+	actual = write(src, sizeToWrite, position);
+	if (actual != -1) position += actual;
+	return actual;
+}
+
+void memPage_t::setNext(const memPage_t* const next){
+	this.next = next;
+}
+
+const memPage_t* memPage_t::getNext(){
+	return next;
+}
---
+++ trunk/hw1/test_page1.cpp
@@ -0,0 +1,33 @@
+#include "memPage_t.h"
+#include <assert.h>
+
+int main(){
+	memPage_t p1;
+	int y;
+	int x = 357;
+
+	assert(p1.getSize() == 0);
+	assert(p1.getCapacity() == 1024);
+	assert(p1.isEmpty());
+	assert(p1.write(&x, sizeof(x)) == sizeof(x));
+	assert(p1.getPosition() == sizeof(x));
+	assert(p1.setPosition(1000) == -1);
+	assert(p1.getPosition() == sizeof(x));
+	assert(p1.read(&y, sizeof(y), 0) == sizeof(y));
+	assert(y == x);
+	assert(p1.setPosition(3) == 3);
+	assert(p1.getPosition() == 3);
+
+	char[] s1 = "arbelzingerhagevgever";
+	char* s2 = new char[sizeof(s1)+1];
+
+	assert(p1.write(s1, sizeof(s1),4) == sizeof(s1));
+	assert(p1.getPosition() == 3);
+	assert(p1.setPosition(4) == 4);
+	assert(p1.read(s2, sizeof(s1)) == sizeof(s1));
+	while (p1.write(s1,sizeof(s1)) > 0) { continue; }
+	assert(p1.isFull());
+	assert(p1.getCapacity() == p1.getSize());
+
+// Add set/get next tests and read/write extremes
+}
