a5bbbf20f04834f8eefeebf6f944d8121447f5f1
ehudt71 <ehudt71@5d74b673-4c72-fe7f-821c-aa23a92225c5>
2013-03-13 21:28:07 +0000

    added comments regarding operator= and deleted memPage_t class from memPool_t.h

--- trunk/hw1/memPool_t.h
+++ trunk/hw1/memPool_t.h
@@ -1,29 +1,5 @@
-class memPage_t {
-	public: // member functions
-		memPage_t(); // Default CTOR
-		memPage_t(const int size); // CTOR with specific size
-		~memPage_t();
-		const int setPosition(const int offset); // Set the current position. Returns the new position or -1 on error
-		const int getPosition() const; // Get the current position
-		const bool isEmpty() const; 
-		const bool isFull() const;
-		const int getSize() const;
-		const int getCapacity() const;
-		const int read(void* const dst, const int size) const; // returns the number of bytes read or -1 on error
-		const int read(void* const dst, const int size, const int offset) const; // returns the number of bytes read or -1 on error
-		const int write(const void* const src, const int size); // returns the number of bytes written or -1 on error
-		const int write(const void* const src, const int size, const int offset); // returns the number of bytes written or -1 on error
-
-	private: //data members
-		memPage_t(const memPage_t& memPage); // Copying memPage_t is not allowed
-		static int maxSize;
-		int size;	// Actual size of the memPage
-		int capacity;	// memPage capacity	
-		int position; // Current position
-		memPage_t* next; // Pointer to the next page
-};
-
 class memPool_t {
+		//TODO make the operator= private (don't allow copying)
 	public: // member functions
 		memPool_t(); // Default CTOR
 		memPool_t(const int initPageCount); // CTOR from initial empty page count
