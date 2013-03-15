e78dac68860ea21eae2c23ba43b27e0fbed5e33c
zinger.arbel <zinger.arbel@5d74b673-4c72-fe7f-821c-aa23a92225c5>
2013-03-15 11:30:05 +0000

    new mempool!

--- trunk/hw1/memPool_t.h
+++ trunk/hw1/memPool_t.h
@@ -1,10 +1,10 @@
+#include "memPage_t.h"
+
 class memPool_t {
-		//TODO make the operator= private (don't allow copying)
 	public: // member functions
 		memPool_t(); // Default CTOR
 		memPool_t(int initPageCount); // CTOR from initial empty page count
 		~memPool_t(); // DTOR
-		const memPool_t& operator= (const memPool_t& memPool); // Overloading the assignment operator
 		int setPosition(int offset); // Set the current position. Returns the new position or -1 on error
 		int getPosition() const; // Get the current position
 		bool isEmpty() const; 
@@ -12,13 +12,14 @@ class memPool_t {
 		int getCapacity() const;
 		int getPageCount() const;
 		int getPageSize() const;
-		int setPageSize(int newSize);
-		int read(void * const dst, int size) const; // returns the number of bytes read or -1 on error
+		static int setNewPageSize(int newSize);
+		int read(void * const dst, int size); // returns the number of bytes read or -1 on error
 		int write(const void * const src, int size); // returns the number of bytes written or -1 on error
 
 	private: //data members
 		memPool_t(const memPool_t& memPool); // Copying memPool_t is not allowed
-		int pageSize; // Default size of page
+		const memPool_t& operator= (const memPool_t& memPool); // Copying memPool_t is not allowed
+		static int newPageSize; // Default size of page
 		int pageCount;	// Page counter
 		int size;	// Actual size of the memPool
 		int capacity;	// memPool capacity
