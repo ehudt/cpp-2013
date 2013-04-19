7a58fc9b59d633d515d7a15364e10fbb1725cdeb
ehudt71@gmail.com <ehudt71@gmail.com@5d74b673-4c72-fe7f-821c-aa23a92225c5>
2013-04-19 21:10:51 +0000

    First implementation version of Dlist_t Implementation of list node Node_t Changed some of the interface in Container_t Added an enum in Container_t to encode error types

--- hw2/Container_t.h
+++ hw2/Container_t.h
@@ -3,20 +3,23 @@
 
 template <class T>
 class Container_t {
-	public:
-		virtual Container_t() = 0; // Default CTOR // TODO: pure virtual?
-		virtual ~Container_t() = 0; // Pure virtual DTOR
-		virtual Container_t(const Container_t<T>& container) = 0; // Copy CTOR
-		virtual const Container_t& operator= (const Container_t<T>& container) = 0; // Operator =
-		virtual int count() const = 0;
-		virtual T* find(T& element) const = 0;
-		virtual bool insert(T& element) = 0; // TODO: handle exceptions
-		virtual bool append(T& element, int index) = 0; // TODO: handle exceptions
-		virtual bool prepend(T& element, int index) = 0; // TODO: handle exceptions
-		virtual T* remove(T& element) = 0;
-		virtual bool removeAndDelete(T& element) = 0; // TODO: handle exceptions
-		virtual bool removeAll() = 0; // TODO: handle exceptions
-		virtual bool removeAllAndDelete() = 0; // TODO: handle exceptions
+public:
+	enum Error {
+		IndexOutOfBounds,
+	};
+	virtual Container_t() = 0; // Default CTOR // TODO: pure virtual?
+	virtual ~Container_t() = 0; // Pure virtual DTOR
+	virtual Container_t(const Container_t<T>& container) = 0; // Copy CTOR
+	virtual const Container_t& operator= (const Container_t<T>& container) = 0; // Operator =
+	virtual int count() const = 0;
+	virtual T* find(const T& element) const = 0;
+	virtual bool insert(const T& element) = 0; // TODO: handle exceptions
+	virtual bool append(const T& element, int index) throw(Error) = 0; // TODO: handle exceptions
+	virtual bool prepend(const T& element, int index) throw(Error) = 0; // TODO: handle exceptions
+	virtual T* remove(const T& element) = 0;
+	virtual bool removeAndDelete(const T& element) = 0; // TODO: handle exceptions
+	virtual bool removeAll() = 0; // TODO: handle exceptions
+	virtual bool removeAllAndDelete() = 0; // TODO: handle exceptions
 };
 
 #endif
