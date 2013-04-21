440cc64840bd6e7778860b3e411efb25e1d0f8d0
zinger.arbel <zinger.arbel@5d74b673-4c72-fe7f-821c-aa23a92225c5>
2013-04-21 14:40:03 +0000

    array:

--- trunk/hw2/Array_t.h
+++ trunk/hw2/Array_t.h
@@ -7,28 +7,28 @@
 
 template <class T>
 class Array_t : public Container_t<T> {
-	public:
-		Array_t(); // Default CTOR
-		~Array_t(); // Pure virtual DTOR
-		Array_t(const Array_t<T>& array); // Copy CTOR
-		const Array_t& operator= (const Array_t<T>& array); // Operator =
-		T& operator[] (int index) const;
-		int count() const;
-		T* find(const T& element) const;
-		bool insert(T& element);
-		bool append(T& element, int index) throw(typename Container_t<T>::Error);
-		bool prepend(T& element, int index) throw(typename Container_t<T>::Error);
-		T* remove(const T& element);
-		bool removeAndDelete(const T& element);
-		bool removeAll();
-		bool removeAllAndDelete();
+public:
+	Array_t(); // Default CTOR
+	~Array_t(); // Pure virtual DTOR
+	Array_t(const Array_t<T>& array); // Copy CTOR
+	const Array_t& operator= (const Array_t<T>& array); // Operator =
+	T& operator[] (int index) const throw(typename Container_t<T>::Error);
+	int count() const;
+	T* find(const T& element) const;
+	bool insert(T& element);
+	bool append(T& element, int index) throw(typename Container_t<T>::Error);
+	bool prepend(T& element, int index) throw(typename Container_t<T>::Error);
+	T* remove(const T& element);
+	bool removeAndDelete(const T& element);
+	bool removeAll();
+	bool removeAllAndDelete();
 
-	private:
-		T** array;
-		static int initialSize;
-		static int expandValue;
-		int count_; // current number of elements in the array
-		int capacity; // total capacity
+private:
+	T** array;
+	static int initialSize;
+	static int expandValue;
+	int count_; // current number of elements in the array
+	int capacity; // total capacity
 };
 
 
@@ -40,10 +40,10 @@ int Array_t<T>::expandValue = 4;
 
 template <class T>
 Array_t<T>::Array_t() :
-	array(new T*[initialSize]),
-	count_(0),
-	capacity(initialSize)
-		{}
+array(new T*[initialSize]),
+count_(0),
+capacity(initialSize)
+{}
 
 template <class T>
 Array_t<T>::~Array_t() {
@@ -52,9 +52,9 @@ Array_t<T>::~Array_t() {
 
 template <class T>
 Array_t<T>::Array_t(const Array_t& existingArray) :
-	array(new T[existingArray.capacity]),
-	count_(existingArray.count_),
-	capacity(existingArray.capacity)
+array(new T[existingArray.capacity]),
+count_(existingArray.count_),
+capacity(existingArray.capacity)
 {
 	for (int i = 0; i < this->count_; ++i) {
 		this[i] = existingArray[i];
@@ -73,7 +73,7 @@ const Array_t<T>& Array_t<T>::operator =(const Array_t& existingArray) {
 }
 
 template <class T>
-T& Array_t<T>::operator [](int index) const{
+T& Array_t<T>::operator [](int index) const throw(typename Container_t<T>::Error){
 	if (index < 0 || index >= count()) throw Container_t<T>::IndexOutOfBounds;
 	return *(array[index]);
 }
@@ -107,45 +107,37 @@ bool Array_t<T>::insert(T& element){
 		}
 
 		this->capacity += expandValue;
-		this->array = temp;
-		delete[] temp;
-		this->array[this->count()] = &element;
-		this->count_++;
-		return 1;
+		delete[] array;
+		array = temp;
+		return insert(element);
 	}
-	return 0;
 }
 
 template <class T>
 bool Array_t<T>::append(T& element, int index) throw(typename Container_t<T>::Error){
-	if (index > this->count() || index < 0) throw Container_t<T>::IndexOutOfBounds;
-		else {
-			if (index == this->count()){
-				return this->insert(element);
-			}
-			else{
-				T** temp;
-				if (this->count()+1 > this->capacity){
-					temp = new T*[this->capacity+expandValue];
-					this->capacity = capacity+expandValue;
-				}
-				else
-					temp = new T*[this->capacity];
-
-				for (int i=0; i < index+1; i++)
-					temp[i] = this->array[i];
-
-				temp[index+1] = &element;
-
-				for (int i = index+1; i < this->count()+1; i++)
-					temp[i+1] = this->array[i];
-				this->count_++;
-				this->array = temp;
-				delete[] temp;
-				return 1;
-				}
-			}
+	if (index >= this->count() || index < 0) throw Container_t<T>::IndexOutOfBounds;
+	else{
+		T** temp;
+		if (this->count()+1 > this->capacity){
+			temp = new T*[this->capacity+expandValue];
+			this->capacity = capacity+expandValue;
 		}
+		else
+			temp = new T*[this->capacity];
+
+		for (int i=0; i < index+1; i++)
+			temp[i] = this->array[i];
+
+		temp[index+1] = &element;
+
+		for (int i = index+1; i < this->count()+1; i++)
+			temp[i+1] = this->array[i];
+		this->count_++;
+		this->array = temp;
+		delete[] temp;
+		return 1;
+	}
+}
 
 
 
@@ -179,16 +171,16 @@ bool Array_t<T>::prepend(T& element, int index) throw(typename Container_t<T>::E
 			this->array = temp;
 			delete[] temp;
 			return 1;
-			}
 		}
 	}
+}
 
 template <class T>
 T* Array_t<T>::remove(const T& element){
 	int i = 0;
 	while ((*(this->array[i]) != element) && (i < capacity+1)) {
 		i++;
-		}
+	}
 
 	if (i == this->capacity+1) return NULL;
 
