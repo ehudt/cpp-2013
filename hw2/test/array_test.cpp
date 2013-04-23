d6fd0db2a6e48ccdbb64e863b0f1479864275103
zinger.arbel <zinger.arbel@5d74b673-4c72-fe7f-821c-aa23a92225c5>
2013-04-23 07:42:40 +0000

    array+test

--- hw2/test/array_test.cpp
+++ hw2/test/array_test.cpp
@@ -1,34 +1,48 @@
 #include "../Array_t.h"
 #include <cassert>
+#include <iostream>
 
+using namespace std;
 
 void Array_tTest(){
 	Array_t<int> array1;
-	const int limit = 10;
-/*	int* i = new int(5);
-	int* j = new int(3);
-	int* l = new int(2);
-
-array1.insert(*i);
-	assert(array1.find(*i)!=NULL);
-	assert(array1.count()==1);
-	array1.append(*j, 0);
-	cout << array1[0] << ' ' << array1[1] << ' '<< array1.count() << "\n";
-	assert(array1.insert(*l));
-	assert(array1.removeAndDelete(*l));
-	cout << array1[0] << ' ' << array1[1]  << ' ' << array1.count() << "\n";
- */
+	const int limit = 17;
 
 	for (int i = 0; i < limit; ++i) {
 			assert(array1.count() == i);
 			int *num = new int(i);
-			if (i % 2) {
+			if (i % 3 == 1) {
 				assert(array1.append(*num, i - 1));
 			}
 			else {
 				assert(array1.insert(*num));
 			}
 		}
-		assert(array1.count() == limit);
-		array1.removeAllAndDelete();
+
+	int *j = new int(100);
+	int *l = new int(103);
+	array1.prepend(*j, 3);
+	assert(array1.count() == limit+1);
+	assert(array1.find(100)==j);
+	array1.prepend(*l, 3);
+	assert(array1.remove(103));
+	assert(array1.count() == limit+1);
+	assert(array1.removeAndDelete(100));
+	assert(array1.count() == limit);
+
+	Array_t<int> array2 = array1;
+	assert(array2.count()==array1.count());
+	assert(array1[3] == array2[3]);
+
+	int *k = new int(102);
+	array2.insert(*k);
+	array2 = array1;
+	assert(array2.count()==array1.count());
+	for (int i = 0; i < array1.count(); i++){
+		assert(array1[i]==array2[i]);
+	}
+	delete k;
+	delete l;
+	array1.removeAllAndDelete();
 }
+
