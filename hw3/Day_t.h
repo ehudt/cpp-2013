5c186a3337d755ba8fa8a9cb013e6f0b84703f49
zinger.arbel <zinger.arbel@5d74b673-4c72-fe7f-821c-aa23a92225c5>
2013-05-01 15:13:36 +0000

    a

--- hw3/Day_t.h
+++ hw3/Day_t.h
@@ -1,7 +1,22 @@
-class Day{
-public:
-	Day();
-	~Day();
+#ifndef _DAY_T_H
+#define _DAY_T_H
+#include <map>
+#include "Appointment_t.h"
 
+using namespace std;
+
+
+class Day_t{
+public:
+	Day_t();
+	~Day_t();
+
+	const Day_t& operator= (const Day_t& day);
+
+private:
+	map<Hour_t, Appointment_t> appointments;
+	int day;
 
 };
+
+#endif
