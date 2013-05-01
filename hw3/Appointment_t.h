f533f6a9c5f2d7e7660a1a97f6d17ddcc5a272a3
zinger.arbel <zinger.arbel@5d74b673-4c72-fe7f-821c-aa23a92225c5>
2013-05-01 15:11:57 +0000

    s

--- hw3/Appointment_t.h
+++ hw3/Appointment_t.h
@@ -1,22 +1,24 @@
 #ifndef _APPOINTMENT_T_H
 #define _APPOINTMENT_T_H
-
+#include "Hour_t.h"
 class Appointment_t {
 
 public:
 	Appointment_t();
-	Appointment_t(AppointmentTime_t appTime, char* subject);
+	Appointment_t(Hour_t startHour, Hour_t endHour, char* subject);
 	~Appointment_t();
 
 	const Appointment_t& operator= (const Appointment_t& appointment);
 	const Appointment_t& operator== (const Appointment_t& appointment) const;
 
-	const  AppointmentTime_t gettime() const;
+	const  Hour_t getStartHour() const;
+	const  Hour_t getEndHour() const;
 	const char* getSubject() const;
 	const char** invited() const;
 
 private:
-	AppointmentTime_t  time;
+	Hour_t  startHour;
+	Hour_t  endHour;
 	char subject[];
 };
 
