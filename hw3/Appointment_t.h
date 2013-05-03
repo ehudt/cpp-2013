89da979cb5a307902ce227028080fc4eddd9c848
zinger.arbel <zinger.arbel@5d74b673-4c72-fe7f-821c-aa23a92225c5>
2013-05-03 11:12:29 +0000

    fixed the h file

--- hw3/Appointment_t.h
+++ hw3/Appointment_t.h
@@ -1,22 +1,27 @@
 #ifndef _APPOINTMENT_T_H
 #define _APPOINTMENT_T_H
 #include "AppointmentTime_t.h"
+#include <string>
+
+using namespace std;
+
 class Appointment_t {
 
 public:
-	Appointment_t();
-	Appointment_t(AppointmentTime_t time, char* subject);
-	~Appointment_t();
+	Appointment_t(AppointmentTime_t time, string& subject);
+	Appointment_t(const Appointment_t& other);
+	virtual ~Appointment_t();
 
-	const Appointment_t& operator= (const Appointment_t& appointment);
-	const Appointment_t& operator== (const Appointment_t& appointment) const;
+	Appointment_t& operator= (const Appointment_t& appointment);
+	bool operator== (const Appointment_t& appointment) const;
 
-	const  AppointmentTime_t getTime() const;
-	const char* getSubject() const;
+	const AppointmentTime_t& getTime() const;
+	const string& getSubject() const;
+	bool setTime(const AppointmentTime_t& newTime);
 
 private:
 	AppointmentTime_t appointmentTime;
-	char subject[];
+	string subject;
 };
 
 #endif
