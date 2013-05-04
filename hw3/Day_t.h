18313feb3de0852bd4b477a87ff44b43f6c1e151
zinger.arbel <zinger.arbel@5d74b673-4c72-fe7f-821c-aa23a92225c5>
2013-05-04 12:39:27 +0000

    made copyconstruct and op= public

--- hw3/Day_t.h
+++ hw3/Day_t.h
@@ -10,14 +10,14 @@ class Day_t{
 public:
 	Day_t(int day);
 	virtual ~Day_t();
+	Day_t(const Day_t& other);
+	Day_t& operator= (const Day_t& day);
 
 	virtual bool add(const Appointment_t& appointment);
 	virtual bool remove(Appointment_t& appointment);
 	virtual const Appointment_t& get(AppointmentTime_t& time) const;
 
 private:
-	Day_t(const Day_t& other);
-	Day_t& operator= (const Day_t& day);
 	map<AppointmentTime_t, Appointment_t> appointments;
 	int day;
 };
