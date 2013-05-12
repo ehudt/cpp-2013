865f6698e59e49bbef27999d17511f90efedc1f1
zinger.arbel <zinger.arbel@5d74b673-4c72-fe7f-821c-aa23a92225c5>
2013-05-12 11:49:55 +0000

    new projecT!

---
+++ trunk/hw4/Library_t.h
@@ -0,0 +1,17 @@
+#ifndef _LIBRARY_T_H
+#define _LIBRARY_T_H
+
+using namespace std;
+
+class Library_t{
+public:
+	Library_t(){};
+	virtual ~Library_t();
+	//virtual bool addBook(string name, string author
+
+
+
+private:
+
+
+};
