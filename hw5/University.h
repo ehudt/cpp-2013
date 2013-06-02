fe00a297d713c1f74b5fda0cb74910400cd99ee9
zinger.arbel <zinger.arbel@5d74b673-4c72-fe7f-821c-aa23a92225c5>
2013-06-02 10:59:10 +0000

    z

---
+++ hw5/University.h
@@ -0,0 +1,28 @@
+#ifndef UNIVERSITY_H_
+#define UNIVERSITY_H_
+
+#include "Subject.h"
+
+class University : public Subject{
+public:
+	static University& getObj();
+	int RaisePrice(int newPrice);
+	void CancelLecture(string& courseName);
+
+private:
+	static University university;
+	University();
+	University(University& uni);
+	void operator=(University& rhs);
+
+	size_t price;
+
+};
+
+
+
+
+
+
+
+#endif
