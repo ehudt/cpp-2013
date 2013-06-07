a5a12e3d99a57077139a42e079843cf2c16c6290
ehudt71@gmail.com <ehudt71@gmail.com@5d74b673-4c72-fe7f-821c-aa23a92225c5>
2013-06-07 11:53:53 +0000

    e

---
+++ hw5/StudentFactory_test.cpp
@@ -0,0 +1,34 @@
+/*
+ * StudentFactory_test.cpp
+ *
+ *  Created on: Jun 7, 2013
+ *      Author: ehudt
+ */
+
+#include <assert.h>
+
+#include "StudentFactory.h"
+#include "University.h"
+
+void testStudentFactory() {
+	University& uni = University::GetObj();
+	Student* student;
+	student = StudentFactory::Create("FOH", "Arbel", &uni);
+	assert(student != 0);
+	assert(student->GetFaculty() == "FOH");
+	assert(student->GetName() == "Arbel");
+
+	student = StudentFactory::Create("ESF", "Ehud", &uni);
+	assert(student != 0);
+	assert(student->GetFaculty() == "ESF");
+	assert(student->GetName() == "Ehud");
+
+	student = StudentFactory::Create("No Existent Faculty", "Dani Dean", &uni);
+	assert(student == 0);
+}
+
+int main(void) {
+	testStudentFactory();
+	return 0;
+}
+
