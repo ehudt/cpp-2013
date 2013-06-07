44f6ae560f97551648c9832809af9dc65886873d
zinger.arbel <zinger.arbel@5d74b673-4c72-fe7f-821c-aa23a92225c5>
2013-06-07 08:04:46 +0000

    s

--- hw5/University.cpp
+++ hw5/University.cpp
@@ -14,12 +14,33 @@ University& University::getObj() {
 
 int University::RaisePrice(int newPrice) {
 	price = newPrice;
-	Notify();
+	Notify(PRICE);
 	return price;
 }
 
 void University::CancelLecture(const string& courseName) {
-	Notify(courseName);//TODO
+	Notify(LECTURE_CANCELATION);
+}
+
+const string& University::getLastMassage() {
+	return lastMassage;
+}
+
+size_t University::getPrice() {
+	return price;
+}
+
+void University::Notify(Notification notification) {
+	if (notification == PRICE){
+		for (int i = 0; i < m_observers.size(); i++)
+			(m_observers[i])->Update(this);
+	}
+	else{
+		for (int i = 0; i < m_observers.size(); i++){
+			if (m_observers[i].getFaculty() == "ESF")
+				(m_observers[i])->Update(this);
+		}
+	}
 }
 
 University::University(int iPrice):price(iPrice){}
--- hw5/University.h
+++ hw5/University.h
@@ -4,18 +4,27 @@
 #include "Subject.h"
 #include "Student.h"
 
+enum Notification {
+	PRICE,
+	LECTURE_CANCELATION
+};
+
 class University : public Subject {
 public:
 	static University& getObj();
 	int RaisePrice(int newPrice);
 	void CancelLecture(const string& courseName);
+	const string& getLastMassage();
+	size_t getPrice();
+	void Notify(Notification notification);
+
 
 private:
 	static University university;
 	University(int iPrice);
 	University(University& uni);
 	void operator=(University& rhs);
-
+	string& lastMassage;
 	size_t price;
 
 };
