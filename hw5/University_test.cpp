b8664b8fc958c8028454c218e3177c82dda7de9e
zinger.arbel <zinger.arbel@5d74b673-4c72-fe7f-821c-aa23a92225c5>
2013-06-07 11:24:38 +0000

    ds

---
+++ hw5/University_test.cpp
@@ -0,0 +1,11 @@
+#include "University.h"
+#include <assert.h>
+
+int main(void){
+	University uni = University::GetObj();
+	uni.CancelLecture("spanish is canceled");
+	assert(uni.GetLastMessage()=="spanish is canceled");
+	uni.RaisePrice(500);
+	assert(uni.GetPrice()==500);
+
+}
